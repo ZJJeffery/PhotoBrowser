ceae43966abdf51c847760f9203d5783a3b288e6
ZJJeffery <359191701@qq.com>
2015-05-25 22:26:58 +0800

    合并成为一个文件

--- 照片查看器/照片查看器/PhotoBrowserScanViewController.swift
+++ 照片查看器/照片查看器/PhotoBrowserScanViewController.swift
@@ -8,14 +8,25 @@
 import UIKit
 import SDWebImage
 import SVProgressHUD
-// 通知列表
+//MARK: - 常量列表
+/// 通知列表
 // 普通dismiss通知
-let PhotoBrowserStartDismissNotification = "PhotoBrowserStartDismissNotification"
-let PhotoBrowserEndDismissNotification = "PhotoBrowserEndDismissNotification"
+private let PhotoBrowserStartDismissNotification = "PhotoBrowserStartDismissNotification"
+private let PhotoBrowserEndDismissNotification = "PhotoBrowserEndDismissNotification"
 // 交互式dismiss通知
-let PhotoBrowserStartInteractiveDismissNotification = "PhotoBrowserStartInteractiveDismissNotification"
+private let PhotoBrowserStartInteractiveDismissNotification = "PhotoBrowserStartInteractiveDismissNotification"
+/// 交互时颜色变化通知
+private let PhotoBrowserDidScaleNotification = "PhotoBrowserDidScaleNotification"
 
-private let reusedId = "photoCell"
+///重用id
+private let reuseIdentifier = "PhotoBrowserCellCell"
+private let reusedId = "PhotoCell"
+
+/// 触发dismiss的Scale大小
+private let dismissScale : CGFloat = 1.0
+
+
+//MARK: - 展示小图的控制器
 class PhotoBrowserScanViewController: UIViewController {
     //MARK: 属性
     /// 布局约束
@@ -46,7 +57,7 @@ class PhotoBrowserScanViewController: UIViewController {
     /// 展开后frame
     var endFrameList : [CGRect]?
     
-    /// 可自定义属性
+    //MARK: - 可自定义属性
     /// 单张图片大小 如果没有给定该参数，单张图片显示的时候就按照layout的大小的2倍显示
     var singleImageSize : CGSize?
     /// 图片间距 默认为 10
@@ -56,12 +67,13 @@ class PhotoBrowserScanViewController: UIViewController {
     // 动画时长
     var AnimationDuration : NSTimeInterval = 3
     
+    //MARK: - 布局约束
     /// 高度约束
     var collectionViewHeight : NSLayoutConstraint?
     /// 宽度约束
     var collectionViewWidth : NSLayoutConstraint?
     
-    //MARK: - 自己方法
+    //MARK: - 系统方法
     override func loadView() {
         view = self.collectionView!
         // 添加约束
@@ -75,6 +87,7 @@ class PhotoBrowserScanViewController: UIViewController {
     deinit{
         removeNotification()
     }
+    //MARK: - 功能方法
     // 销毁注册的通知
     private func removeNotification(){
         NSNotificationCenter.defaultCenter().removeObserver(self)
@@ -381,3 +394,374 @@ extension PhotoBrowserScanViewController : UICollectionViewDelegate {
     }
 }
 
+
+//MARK: - 单图展示Cell
+class PhotoBrowserCell: UICollectionViewCell {
+    //MARK: - 属性
+    /// 查看照片控制器
+    var viewerVC: SinglePhotoBrowserViewController?
+    
+    /// 要显示的图片的 URL
+    var largeURL: NSURL? {
+        didSet {
+            viewerVC?.largeURL = largeURL
+        }
+    }
+    /// 要显示的图片的小图 URL
+    var smallURL: NSURL? {
+        didSet {
+            viewerVC?.smallURL = smallURL
+        }
+    }
+    /// 要显示的图片的小图 URL
+    var index: Int? {
+        didSet {
+            viewerVC?.index = index
+        }
+    }
+    //MARK: - 构造方法
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        // 创建视图控制器
+        viewerVC = SinglePhotoBrowserViewController()
+        viewerVC?.view.frame = bounds
+        // 添加视图
+        self.addSubview(viewerVC!.view)
+    }
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
+//MARK: - 处理单张图片的控制器
+class SinglePhotoBrowserViewController: UIViewController {
+    //MARK: - 属性
+    /// 滚动视图
+    lazy var scrollView : UIScrollView = {
+        let sv = UIScrollView(frame: UIScreen.mainScreen().bounds)
+        // 设置代理
+        sv.delegate = self
+        // 最小大缩放比例
+        sv.minimumZoomScale = 0.5
+        sv.maximumZoomScale = 2.0
+        return sv
+        }()
+    /// 图像视图
+    lazy var imageView : UIImageView = {
+        let iv = UIImageView()
+        return iv
+        }()
+    var largeURL : NSURL? {
+        didSet {
+            // 清除原有的图片
+            // 判断图片是否缓存
+            if !(SDWebImageManager.sharedManager().cachedImageExistsForURL(largeURL)) {
+                SDWebImageManager.sharedManager().downloadImageWithURL(smallURL, options: SDWebImageOptions(0), progress: nil, completed: { (image, error, _, _, _) -> Void in
+                    self.imageView.image = image
+                    self.setUpImage(image)
+                })
+            }
+            SDWebImageManager.sharedManager().downloadImageWithURL(largeURL, options: SDWebImageOptions(0), progress: nil) { (image, error, _, _, _) -> Void in
+                if error != nil {
+                    SVProgressHUD.showErrorWithStatus("网络不给力")
+                    SVProgressHUD.dismiss()
+                    return
+                }
+                // 设置图片
+                self.imageView.image = image
+                self.setUpImage(image)
+                SVProgressHUD.dismiss()
+            }
+        }
+    }
+    // 小图URL
+    var smallURL : NSURL?
+    // 当前索引
+    var index : Int?
+    
+    //MARK: - 内部方法
+    override func loadView() {
+        view = UIView(frame: UIScreen.mainScreen().bounds)
+        view.addSubview(scrollView)
+        scrollView.addSubview(imageView)
+        // 设置SVProgressHUD
+        setSVProgressHUD()
+    }
+    override func viewDidDisappear(animated: Bool) {
+        super.viewDidDisappear(animated)
+        SVProgressHUD.dismiss()
+    }
+    //MARK: - 功能方法
+    // 设置SVProgressHUD样式
+    private func setSVProgressHUD(){
+        SVProgressHUD.setBackgroundColor(UIColor(red: 0, green: 0, blue: 0, alpha: 0.5))
+        SVProgressHUD.setForegroundColor(UIColor.whiteColor())
+        SVProgressHUD.setRingThickness(8.0)
+    }
+    // 重置scrollView属性
+    private func resetScrollView() {
+        scrollView.contentSize = CGSizeZero
+        scrollView.contentOffset = CGPointZero
+        scrollView.contentInset = UIEdgeInsetsZero
+        imageView.transform = CGAffineTransformIdentity
+    }
+    // 设置图片
+    private func setUpImage(image : UIImage) {
+        // 重置
+        resetScrollView()
+        // 缩放大小
+        let size = scaleImageSize(image)
+        // 设置属性
+        self.imageView.frame = CGRectMake(0, 0, size.width, size.height)
+        // 判断长短图
+        var top : CGFloat
+        if size.height > view.frame.height {
+            // 长图
+            top = 0
+        } else {
+            // 短图
+            top = (view.frame.size.height - size.height) * 0.5
+        }
+        // 设置contentInset
+        scrollView.contentSize = size
+        scrollView.contentInset = UIEdgeInsetsMake(top, 0, 0, 0)
+    }
+    // 计算图片缩放
+    private func scaleImageSize(image : UIImage) -> CGSize {
+        let imageW = image.size.width
+        let imageH = image.size.height
+        let screenW = UIScreen.mainScreen().bounds.width
+        let scaleH = screenW * imageH / imageW
+        return CGSizeMake(screenW, scaleH)
+    }
+    // 判断图片的缩放便宜
+    private func calculateContentOffset() -> CGPoint{
+        // 根据图像的目前大小计算需要偏移的contentOffset
+        var offsety : CGFloat
+        var offsetx : CGFloat
+        // 判断图片的高度是否已经超出屏幕，超出屏幕按照默认的contentOffset
+        if imageView.frame.height > UIScreen.mainScreen().bounds.height {
+            offsety = scrollView.contentOffset.y //y值不变，直接保持即可
+        }else{
+            offsety = (imageView.frame.height - UIScreen.mainScreen().bounds.height) * CGFloat(0.5)
+        }
+        
+        // 判断图片的宽度是否已经超出屏幕，超出屏幕按照默认的contentOffset
+        if imageView.frame.width > UIScreen.mainScreen().bounds.width {
+            offsetx = scrollView.contentOffset.x //y值不变，直接保持即可
+        }else{
+            offsetx = (imageView.frame.width - UIScreen.mainScreen().bounds.width) * CGFloat(0.5)
+        }
+        return CGPointMake(offsetx, offsety)
+    }
+}
+//MARK: - UIScrollViewDelegate代理方法
+extension SinglePhotoBrowserViewController : UIScrollViewDelegate {
+    func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    
+    func scrollViewDidZoom(scrollView: UIScrollView) {
+        let scale = imageView.transform.a
+        // 根据偏移量保持图片中心对齐
+        scrollView.contentOffset = calculateContentOffset()
+        // 发送缩放通知
+        NSNotificationCenter.defaultCenter().postNotificationName(PhotoBrowserDidScaleNotification, object: nil, userInfo: ["scale" : scale, "index" : index!])
+    }
+    
+    func scrollViewWillEndDragging(scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        if imageView.transform.a < dismissScale {
+            imageView.hidden = true
+            scrollView.contentOffset = calculateContentOffset()
+        }
+    }
+    func scrollViewDidEndZooming(scrollView: UIScrollView, withView view: UIView!, atScale scale: CGFloat) {
+        if scale < dismissScale {
+            NSNotificationCenter.defaultCenter().postNotificationName(PhotoBrowserStartInteractiveDismissNotification, object: nil, userInfo: ["scale" : scale, "index" : index!])
+            dismissViewControllerAnimated(false, completion: nil)
+            return
+        }
+        // 重新调整图像的间距
+        // 计算顶部的间距值
+        let top = (scrollView.frame.height - view.frame.height) * 0.5
+        if top > 0 {
+            scrollView.contentInset = UIEdgeInsetsMake(top, 0, 0, 0)
+            return
+        }
+        // top < 0 说明图片放大的结果，已经超出了 scrollView
+        scrollView.contentInset = UIEdgeInsetsMake(0, 0, 0, 0)
+    }
+}
+//MARK: - 展示全部图片的控制器
+class PhotoBrowserViewController: UIViewController {
+    //MARK: - 属性
+    /// 布局属性
+    lazy var layout : UICollectionViewFlowLayout = {
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.itemSize = self.view.frame.size
+        flowLayout.minimumLineSpacing = 0
+        flowLayout.minimumInteritemSpacing = 0
+        flowLayout.scrollDirection = UICollectionViewScrollDirection.Horizontal
+        return flowLayout
+        }()
+    /// collectionView
+    lazy var collectionView : UICollectionView = {
+        let cv = UICollectionView(frame: self.view.bounds, collectionViewLayout: self.layout)
+        cv.backgroundView = UIView(frame: self.view.bounds)
+        cv.backgroundColor = UIColor.clearColor()
+        // 设置背景视图的颜色，为了之后对于不同的颜色可以通过调节视图的透明度来实现颜色渐变
+        cv.backgroundView?.backgroundColor = UIColor.blackColor()
+        // 设置数据源代理
+        cv.dataSource = self
+        // 设置其他属性
+        cv.pagingEnabled = true
+        cv.showsHorizontalScrollIndicator = false
+        return cv
+        }()
+    /// 关闭按钮
+    lazy var closeBtn : UIButton = {
+        return self.createButton("关闭")
+        }()
+    /// 大图URL数组
+    var largeImageURLList : [NSURL]?
+    /// 大图URL数组
+    var smallImageURLList : [NSURL]?
+    /// 浏览的位置
+    var index : Int?
+    /// 所有frame对于主视图的位置
+    var startFrameList : [CGRect]?
+    var endFrameList : [CGRect]?
+    
+    //MARK: - 内部方法
+    override func loadView() {
+        let frame = CGRectMake(0, 0, UIScreen.mainScreen().bounds.width + 20, UIScreen.mainScreen().bounds.height)
+        view = UIView(frame: frame)
+        view.backgroundColor = UIColor.clearColor()
+        view.addSubview(self.collectionView)
+        // 创建约束
+        var cons = [AnyObject]()
+        cons += NSLayoutConstraint.constraintsWithVisualFormat("H:|-20-[btn(80)]", options: NSLayoutFormatOptions(0), metrics: nil, views: ["btn" : closeBtn])
+        
+        cons += NSLayoutConstraint.constraintsWithVisualFormat("V:[btn]-20-|", options: NSLayoutFormatOptions(0), metrics: nil, views: ["btn" : closeBtn])
+        // 添加约束
+        view.addConstraints(cons)
+        
+        // 监听方法
+        closeBtn.addTarget(self, action: "close", forControlEvents: UIControlEvents.TouchUpInside)
+    }
+    override func viewDidLoad() {
+        // 注册cell
+        collectionView.registerClass(PhotoBrowserCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        // 注册通知
+        registerNotification()
+    }
+    override func viewDidLayoutSubviews() {
+        let indexPath = NSIndexPath(forItem: index!, inSection: 0)
+        collectionView.scrollToItemAtIndexPath(indexPath, atScrollPosition: UICollectionViewScrollPosition.allZeros, animated: false)
+    }
+    // 销毁通知
+    deinit{
+        NSNotificationCenter.defaultCenter().removeObserver(self)
+    }
+    //MARK: - 监听方法
+    /// 关闭视图
+    func close() {
+        SVProgressHUD.dismiss()
+        // 确定关闭的图像索引
+        let indexPath = collectionView.indexPathsForVisibleItems().last as! NSIndexPath
+        let index = indexPath.item
+        let cell = collectionView.cellForItemAtIndexPath(indexPath)
+        self.view.alpha = 0
+        // 发送开始关闭通知
+        NSNotificationCenter.defaultCenter().postNotificationName(PhotoBrowserStartDismissNotification, object: nil, userInfo: ["index": index])
+    }
+    func didDismiss(){
+        dismissViewControllerAnimated(false, completion: nil)
+    }
+    //MARK: - 功能方法
+    /// 所有注册的通知
+    private func registerNotification(){
+        // 注册通知
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "didDismiss", name: PhotoBrowserEndDismissNotification, object: nil)
+        // 注册通知
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "didScale:", name: PhotoBrowserDidScaleNotification, object: nil)
+    }
+    /// 创建按钮
+    private func createButton(title: String) -> UIButton {
+        let btn = UIButton()
+        btn.setTitle(title, forState: UIControlState.Normal)
+        btn.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
+        btn.backgroundColor = UIColor.brownColor()
+        view.addSubview(btn)
+        // 设置自动布局须关闭
+        btn.setTranslatesAutoresizingMaskIntoConstraints(false)
+        return btn
+    }
+    /// 开始缩放的通知方法
+    func didScale(noti : NSNotification){
+        let scale = noti.userInfo!["scale"] as! CGFloat
+        // 隐藏关闭按钮
+        closeBtn.hidden = scale < 1.0
+        collectionView.backgroundView?.alpha = scale
+    }
+}
+//MARK: - PhotoBrowserViewController的数据源方法
+extension PhotoBrowserViewController: UICollectionViewDataSource {
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return largeImageURLList?.count ?? 0
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as! PhotoBrowserCell
+        // 添加子控制器
+        if !(childViewControllers as NSArray).containsObject(cell.viewerVC!) {
+            addChildViewController(cell.viewerVC!)
+        }
+        // 设置图片URL
+        cell.smallURL = smallImageURLList![indexPath.item]
+        cell.largeURL = largeImageURLList![indexPath.item]
+        // 传递当前索引
+        cell.index = indexPath.item
+        return cell
+    }
+}
+
+//MARK: - 展示单张图片的cell
+class PhotoCell: UICollectionViewCell {
+    //MARK: - 属性
+    /// 展示图像视图
+    lazy var imageView : UIImageView? = {
+        let imageView = UIImageView()
+        imageView.contentMode = UIViewContentMode.ScaleAspectFill
+        imageView.clipsToBounds = true
+        self.addSubview(imageView)
+        return imageView
+        }()
+    /// 图像地址
+    var url : NSURL? {
+        didSet {
+            imageView!.sd_setImageWithURL(url)
+        }
+    }
+    // 功能方法
+    // 判断长短图
+    private func isLongImage(image : UIImage) -> Bool{
+        let size = scaleImageSize(image, relateToWidth: UIScreen.mainScreen().bounds.width)
+        if size.height > UIScreen.mainScreen().bounds.height {
+            return true
+        }
+        return false
+    }
+    // 计算图片缩放根据给定的宽度
+    private func scaleImageSize(image : UIImage, relateToWidth width: CGFloat) -> CGSize {
+        let imageW = image.size.width
+        let imageH = image.size.height
+        let scaleH = width * imageH / imageW
+        return CGSizeMake(width, scaleH)
+    }
+    //MARK: - 内部方法
+    // 布局内部图像视图
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        imageView?.frame = self.bounds
+    }
+}
